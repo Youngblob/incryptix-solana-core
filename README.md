00758dca26dfbff2237c541b09cfb9b7a8e43eba
satoshi <satoshi@pop-os.localdomain>
2021-08-28 22:52:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# solana program for youngblog
+
+it contains a test program for now
+
+source code at /src/program-c/src/helloworld/helloworld.c
---
+++ dist/program/helloworld/helloworld.d
@@ -0,0 +1,3 @@
+../../dist/program/helloworld/helloworld.o: src/helloworld/helloworld.c \
+  /home/satoshi/.local/share/solana/install/active_release/bin/sdk/bpf/c/inc/solana_sdk.h \
+  /home/satoshi/.local/share/solana/install/active_release/bin/sdk/bpf/c/../dependencies/bpf-tools/llvm/lib/clang/12.0.1/include/stdbool.h
